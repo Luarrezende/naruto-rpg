a4d146853f372798f0f2e0b680654a2ba7d43a5c
Luar <luarrezende@gmail.com>
2023-08-02 16:23:00 -0300

    finaliza pagina de login

---
+++ naruto-rpg/src/pages/Login.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+function Login() {
+  const history = useHistory();
+
+  const [login, setLogin] = useState({
+    name: '',
+    password: '',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setLogin({
+      ...login,
+      [name]: value,
+    });
+  };
+
+  const handleClick = () => {
+    localStorage.setItem('user', JSON.stringify({ email: login.email }));
+    history.push('/mainpage');
+  };
+
+  const MIN_PASSWORD = 5;
+  const MIN_NAME = 3;
+  const verify = login.password.length >= MIN_PASSWORD && login.name.length >= MIN_NAME;
+
+  return (
+    <div className='center'>
+        <h1 className='login'>Login</h1>
+        <input
+          type='text'
+          placeholder='Nome'
+          className='inputLogin'
+          name="name"
+          value={ login.name }
+          onChange={ handleChange }
+        />
+        <input
+          type='password'
+          placeholder='Senha'
+          className='inputLogin'
+          name="password"
+          value={ login.password }
+          onChange={ handleChange }
+        />
+        <button
+          className="buttonLogin"
+          type="button"
+          data-testid="login-submit-btn"
+          disabled={ !verify }
+          onClick={ handleClick }
+        >
+            Enter
+        </button>
+        <Link to="/register" className='linkLogin'>Ainda não tem registro?</Link>
+    </div>
+  )
+}
+
+export default Login
