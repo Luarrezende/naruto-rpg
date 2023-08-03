e52dd7f2d015baba14cec2eb7c45620e2d3e9817
Luar <luarrezende@gmail.com>
2023-08-03 02:01:44 -0300

    cria parte grafica de dinheiro na mainPage

--- naruto-rpg/src/pages/MainPage.js
+++ naruto-rpg/src/pages/MainPage.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import CardArmas from '../components/CardArmas'
+import { GrMoney } from 'react-icons/gr';
 
 
 function MainPage() {
   return (
     <div className='background'>
       <h1 className='title'>Main Page</h1>
+      <div className='mainMoney'>
+        <GrMoney className='iconMoney' />
+        <p className='numMoney'>0</p>
+      </div>
       <CardArmas />
       <Footer />
     </div>
